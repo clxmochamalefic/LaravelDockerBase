ae4229d42f027325a6df89a46dd1f6a49e6d5137
ost-itc-j-akakura <n-akakura@ostechnology.co.jp>
2023-06-05 11:24:31 +0900

    bugfix docker_compose_wrapped.ps1

--- docker_compose_wrapped.ps1
+++ docker_compose_wrapped.ps1
@@ -1,6 +1,7 @@
 Param(
-  [switch]$up,    # docker-compose up
-  [switch]$down,  # docker-compose down
+  [switch]$up,
+  [switch]$down,
+  [switch]$n
 )
 
 $nginx_default_conf = "\docker\nginx\.default.conf"
@@ -8,7 +9,7 @@ $nginx_conf = "\docker\nginx\default.conf"
 
 # 設定読み込み
 $ini_path = @(Split-Path $script:myInvocation.MyCommand.Path -Parent).Trim()
-$ini_file = ".env.default"
+$ini_file = ".env"
 $ini_fullname = $ini_path + "\" + $ini_file
 
 $parameter = @{}
@@ -22,14 +23,17 @@ $nginx_conf_path          = $ini_path + "\" + $nginx_conf
 
 # laravelプロジェクト名を変更
 if ($laravel_project_name -ne "public") {
-  (Get-Content $nginx_conf_default_path) | foreach { $_ -replace "root /var/www/public/public","root /var/www/" + laravel_project_name + "/public" } | Set-Content $nginx_conf_path
+  $replaced = "root /var/www/" + $laravel_project_name + "/public"
+  (Get-Content $nginx_conf_default_path) | foreach { $_ -replace "root /var/www/public/public",$replaced } | Set-Content $nginx_conf_path
 }
 
 # docker/nerdctl サブコマンド指定
 $subcommand = ""
+$option = ""
 
 if ($up) {
   $subcommand = "up"
+  $option = "-d"
 } elseif ($down) {
   $subcommand = "down"
 } else {
@@ -37,9 +41,18 @@ if ($up) {
   exit 1
 }
 
+$dcyml = $ini_path + "/docker-compose.yml"
+
 try {
-  docker-compose -p $compose_project_name $subcommand -d
+  if ($n) {
+    nerdctl compose -p $compose_project_name -f $dcyml $subcommand $option
+  } else {
+    docker-compose -p $compose_project_name $subcommand $option
+  }
 } catch {
-  $dcyml = $ini_path + "/docker-compose.yml"
-  nerdctl compose -p $compose_project_name -f $dcyml $subcommand
+  if ($n) {
+    docker-compose -p $compose_project_name $subcommand $option
+  } else {
+    nerdctl compose -p $compose_project_name -f $dcyml $subcommand $option
+  }
 }
